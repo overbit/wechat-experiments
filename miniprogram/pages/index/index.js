b90cfd6ec72d52c5f118b69a6dc15bbd86665585
Daniele Iasella <>
2019-05-02 16:10:49 +0100

    Added miniprogram

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,111 @@
+// pages/home/home.js
+const app = getApp();
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseUserInfo: wx.canIUse('button.open-type.getUserInfo'),
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUseUserInfo){
+      console.info("canIUseUserInfo");
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * Custom functions
+   */
+  signup: function () {
+    wx.navigateTo({
+      url: '../onboarding/signup/signup'
+    })
+  },
+
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }, 
+})
---
+++ miniprogram/pages/onboarding/signup/signup.js
@@ -0,0 +1,121 @@
+// pages/signup/signup.js
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    userInfo: {},
+    userProfile: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  },
+
+  submitForm: function (e) {
+    console.log(e);
+    if (!app.globalData) {
+      app.globalData.userProfile = new Object();
+    }
+    app.globalData.userProfile = e.detail.value;
+    wx.navigateTo({
+      url: '../profession/signup'
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function () {
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // // } else if (this.data.canIUse) {
+    // //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    // //   // 所以此处加入 callback 以防止这种情况
+    // //   app.userInfoReadyCallback = res => {
+    // //     this.setData({
+    // //       userInfo: res.userInfo,
+    // //       hasUserInfo: true
+    // //     })
+    // //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+
+    if (app.globalData.userProfile) {
+      console.log("here")
+      this.setData({
+        userProfile: app.globalData.userProfile
+              })
+      }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
